0e6de193ad58b3335d81707979ac6ad393f68c4d
fred913 <41846274+fredisapig@users.noreply.github.com>
2020-03-06 09:53:51 +0800

    代码编码添加

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 # coding: utf-8
-host = '0.0.0.0'
-port = 7709
+
+from io import open
+from json import load as load_json_file
+
+f = open("config.json", "r")
+cfg = load_json_file(f)
+f.close()
+
+host = "127.0.0.1"
+port = int(cfg['port'])
+uid = str(cfg['uid'])
